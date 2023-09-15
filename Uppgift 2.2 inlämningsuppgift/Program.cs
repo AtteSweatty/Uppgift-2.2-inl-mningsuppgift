21b3ef10a6c987b0dd224b9626149b634300bf7b
arha06001 <arha06001@E-PW0234JA>
2023-09-15 13:08:52 +0200

    Add project files.

---
+++ Uppgift 2.2 inlämningsuppgift/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Atte
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            string långttext = Console.ReadLine();
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            string hoppadetext = Console.ReadLine();
+            double långt = double.Parse(långttext);
+            double hoppade = double.Parse(hoppadetext);
+            double skillnad = långt - hoppade;
+            Console.WriteLine("Elin hoppade " +skillnad+ " meter mer än Alma");
+        }
+    }
+}
